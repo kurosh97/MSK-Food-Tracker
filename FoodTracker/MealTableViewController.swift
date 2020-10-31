8613b0d50f7979d16c96f59392af58100cf462c7
iosdev <mohammhu@metropolia.fi>
2020-10-31 20:12:02 +0200

    one more fix and its good to go

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -10,11 +10,7 @@ import UIKit
 
 class MealTableViewController: UITableViewController {
         
-    
-    
-    
-    
-    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
